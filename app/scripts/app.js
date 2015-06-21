6bdb13ffe367c952932d897b67e42afacbd7e99d
wilsto <willy.stophe.pro@gmail.com>
2015-06-21 16:18:13 +0200

    0.3.5
    Creation de la page accueil

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -89,5 +89,5 @@ angular.module('starter', [
         }
     });
     // if none of the above states are matched, use this as the fallback
-    $urlRouterProvider.otherwise('/app/playlists');
+    $urlRouterProvider.otherwise('/app/intro');
 });
